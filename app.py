2a0b62fc4f196211fe8c4e265b241d7e3b03826b
AmalRL <amal.m@rocketlearning.org>
2026-04-28 14:59:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ else:  # SS Analytics
         ("Matched Users", "total_matched_users"),
         ("Onboarded Users", "onboarding_users"),
         ("% Onboarded Users out of Matched User", "onboarding_percentage", "%"),
-        ("Age ≤ 36 Months out of Onboarded users", "age_36_users"),
+        ("Age ≤ 36 Months out of DB users", "age_36_users"),
         ("Activated Users out of Onboarded Users", "activated_users"),
         ("% Activated Users out of Onboarded Users", "activated_percentage", "%"),
         ("Last week WAU out of Activated Users", "wau_users"),
